637a269836296abd1f24d5578d678fe5ea51bf65
dpeng <dongfeng.peng@nokia-sbell.com>
2021-02-20 12:32:03 +0800

    add codecBurn project

---
+++ codecBurn/ffplay.h
@@ -0,0 +1,6 @@
+#ifndef _FF_PLAY_H_
+#define _FF_PLAY_H_
+void playMpegFile(char* fileName, RECT* rc, char* variable);
+void playPause();
+void playClose();
+#endif
---
+++ codecBurn/newFfPlay/ffPlay.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <windows.h>
+
+class CffPlay
+{
+public:
+	CffPlay(void);
+	~CffPlay(void);
+	void playMpegFile(char* fileName, RECT* rc, char* variable);
+	void playPause();
+	void playClose();
+};
